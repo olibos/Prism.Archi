c961a690ec6181729e7b4e7fc374d1603f49c154
Simon Baudart <s.baudart@wavenet.be>
2020-08-27 14:46:55 +0200

    Very small change :)

--- Prism.Archi.PerformanceComparison/Tests/Collections/FindInDictionary.cs
+++ Prism.Archi.PerformanceComparison/Tests/Collections/FindInDictionary.cs
@@ -21,7 +21,7 @@ namespace Prism.Archi.PerformanceComparison.Tests.Collections
             for (var i = 0; i < this.hits; i++)
             {
                 var id = this.dice.Next(0, this.itemsCount).ToString();
-                var success = dictionary.TryGetValue(id, out Foo found);
+                var success = dictionary.TryGetValue(id, out var found);
             }
         }
     }
