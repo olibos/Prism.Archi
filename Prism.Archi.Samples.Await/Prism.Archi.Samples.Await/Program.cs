25cf7cce69a558dde0bbb6e9732c08189d20580b
Simon Baudart <s.baudart@wavenet.be>
2020-09-03 10:37:12 +0200

    Create the base project

---
+++ Prism.Archi.Samples.Await/Prism.Archi.Samples.Await/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="Prism">
+// Copyright (c) Prism. All rights reserved.
+// </copyright>
+
+namespace Prism.Archi.Samples.Await
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Prism.Archi.Samples.Await/Prism.Archi.Samples.Await/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Prism.Archi.Samples.Await")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prism.Archi.Samples.Await")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F07B9C2E-220E-4373-803F-872C2458CDC2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
